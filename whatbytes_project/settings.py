06e468473c49fc8fcaf3b2fc1400473827040563
John Doe <gajeet11000@gmail.com>
2024-12-21 15:36:58 +0530

    removed smtp configs

--- whatbytes_project/settings.py
+++ whatbytes_project/settings.py
@@ -136,7 +136,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = "587"
 EMAIL_USE_TLS = 'True'
-
-EMAIL_HOST_USER = "gajeet11000@gmail.com"
-EMAIL_HOST_PASSWORD = "mqcp cmsi wkuh bxte"
-DEFAULT_FROM_EMAIL = "gajeet11000@gmail.com"
